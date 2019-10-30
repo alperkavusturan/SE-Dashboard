9ed03704b8ea02e2baac4e27c9474fdb8d22aaf1
Alper Kavuşturan <bilgi@alperkavusturan.com>
2019-10-30 17:34:34 +0300

    fixes

--- src/app/services/artist-service/artist.ts
+++ src/app/services/artist-service/artist.ts
@@ -1,9 +1,9 @@
-export class Artist{
-    
-    public id : number
-    public name : string
-    public surname : string
-    public nickname : string
-    public artistTypeId : number
-    
+export class Artist {
+    constructor(
+        public id: number,
+        public name: string,
+        public surname: string,
+        public nickname: string,
+        public artistTypeId: number
+    ) { }
 }
